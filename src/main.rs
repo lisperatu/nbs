bc63b0071214c2b3a70054a29f0de33113b768da
Sasha Shipka <xaotuk@copyleft.no>
2023-04-16 22:41:17 +0200

    Added docstrings

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
+//! A simple currency quote scraper for Ledger.
+//!
+//! This program provides a currency quote scraper that fetches currency quotes
+//! from specified websites and formats them for the Ledger command-line accounting tool.
+//! It reads configuration from a `.quoteparams` YAML file located in the user's home directory.
+
+#![doc = include_str!("../Readme.md")]
+
+
+
 use chrono::prelude::*;
+use home::home_dir;
+use rayon::prelude::*;
 use std::error::Error;
 use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
-use home::home_dir;
-use rayon::prelude::*;
 
 use serde::*;
 use serde_yaml;
 use std::fmt;
 
-// mod error;
-//use crate::error::CurrencyError;
-
+/// A custom error type for handling currency-related errors.
 #[derive(Debug)]
 struct CurrencyError(String);
 
@@ -25,17 +33,29 @@ impl fmt::Display for CurrencyError {
 impl Error for CurrencyError {}
 
 impl From<String> for CurrencyError {
-    fn from(str: String) -> Self { Self(str) }
+    fn from(str: String) -> Self {
+        Self(str)
+    }
 }
 
+/// A struct representing a set of quote parameters.
 #[derive(Deserialize, Debug)]
 struct QuoteParams {
     url: String,
     select: String,
     from: String,
-    to: String
+    to: String,
 }
 
+/// Reads the quote parameters from a specified path.
+///
+/// # Arguments
+///
+/// * `path` - A reference to a `PathBuf` representing the path to the configuration file.
+///
+/// # Returns
+///
+/// * A `Result` containing a `Vec` of `QuoteParams` if successful, or a `CurrencyError` if an error occurs.
 fn read_quote_params(path: &PathBuf) -> Result<Vec<QuoteParams>, CurrencyError> {
     let mut file = File::open(path).map_err(|_| format!("Failed to open file {:?}", path))?;
     let mut content = String::new();
@@ -48,23 +68,34 @@ fn read_quote_params(path: &PathBuf) -> Result<Vec<QuoteParams>, CurrencyError> 
     Ok(params)
 }
 
-                  fn get_currency(param: QuoteParams
-) -> Result<String, CurrencyError> {
+/// Retrieves a currency quote using the provided `QuoteParams`.
+///
+/// # Arguments
+///
+/// * `param` - A `QuoteParams` instance containing the required parameters for retrieving the quote.
+///
+/// # Returns
+///
+/// * A `Result` containing a `String` with the formatted quote if successful, or a `CurrencyError` if an error occurs.
+fn get_currency(param: QuoteParams) -> Result<String, CurrencyError> {
     let res = reqwest::blocking::get(param.url.as_str())
         .map_err(|_| format!("Cannot read url {}", param.url))?;
-    let content = res.text()
+    let content = res
+        .text()
         .map_err(|_| format!("Cannot parse html from url {}", param.url))?;
     let page = scraper::Html::parse_document(&content);
 
     // I couldn't use simple result? syntax here, I couldn't follow why
-    let selector = match scraper::Selector::parse(&param.select) {
-        Ok(selector) => selector,
-        Err(_) => {
-            return Err(CurrencyError(format!("Cannot parse html selector {}", param.select)))
-        }
+    let selector = if let Ok(selector) = scraper::Selector::parse(&param.select) {
+        selector
+    } else {
+        return Err(CurrencyError(format!(
+            "Cannot parse html selector {}",
+            param.select
+        )));
     };
-    
-    let currency = page                     
+
+    let currency = page
         .select(&selector)
         .map(|v| v.inner_html())
         .next()
@@ -74,6 +105,10 @@ fn read_quote_params(path: &PathBuf) -> Result<Vec<QuoteParams>, CurrencyError> 
     Ok(out)
 }
 
+/// The main function of the currency quote scraper.
+///
+/// It reads the configuration file, retrieves the currency quotes in parallel,
+/// and prints the results in a format suitable for Ledger.
 fn main() -> Result<(), Box<dyn Error>> {
     let mut path = home_dir().ok_or("Cannot find home dir")?;
     path.push(".quoteparams");
@@ -87,6 +122,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     for res in results {
         println!("{}", res);
     }
-    
+
     Ok(())
 }
